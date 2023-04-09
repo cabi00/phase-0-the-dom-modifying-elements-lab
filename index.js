b66114678c772106d10cc3137264c95fa21f53e9
vanessa garcia <venessag0908@gmail.com>
2023-04-09 14:10:02 -0600

    modifying DOM lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const body = document.getElementsByTagName("body"); 
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+//document.append(h1);
+//const newHeader = document.getElementsByTagName("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Vanessa is the champion";
